6c2159d24689295baf8d06ce674534b08cb1f5d8
iamrosan <roshanshrestha21.63@gmail.com>
2024-09-06 18:29:52 +0545

    feat: urls updated

--- account/urls.py
+++ account/urls.py
@@ -3,5 +3,8 @@ from .views import *
 urlpatterns = [
     path('',index, name='index'),
     path('students/',get_students, name='get-student'),
-    path('marks/<str:name>',get_subject_marks, name='marks'),
+    path('marks/<str:id>',get_subject_marks, name='marks'),
+    path('result/<str:name>', view_result, name='result'),
+    path('department/', department_result , name='department-result'),
+    path('download-pdf/<str:name>/', download_pdf, name='download_pdf'),
 ]
